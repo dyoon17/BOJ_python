63b5bfcdf28efcf7e0407236dd6953989dcf6228
dyoon17 <nkdy08@gmail.com>
2024-10-05 21:44:43 +0900

    Update rotating_queue.py

--- rotating_queue.py
+++ rotating_queue.py
@@ -1,21 +1,22 @@
 n, m = map(int, input().split())  # 큐의 크기 n, 뽑아낼 원소의 개수 m
-targets = list(map(int, input().split()))  # 뽑아낼 원소의 위치
+positions = list(map(int, input().split()))  # 뽑아낼 원소의 위치에 대한 리스트 생성
 
-queue = list(range(1, n + 1))  # 큐 초기화
-moves = 0  # 이동 횟수 저장
+queue = list(range(1, n + 1))  # 큐 초기화(1부터 n까지)
+moves = 0  # 총 이동 횟수 저장
 
-for target in targets:
+for remove in positions :
     # 목표 원소의 현재 위치
-    pos = queue.index(target)
+    pos = queue.index(remove )
     
-    # 왼쪽으로 이동할 때와 오른쪽으로 이동할 때 중 더 적은 이동 횟수를 선택
+    # 왼쪽으로 이동할 때
     left = pos
+    # 오른쪽으로 이동할 때 (전체 길이에서 현재 위치를 뺀 값)
     right = len(queue) - pos
     
-    # 더 적은 이동 횟수를 moves에 더함
+    # 왼쪽과 오른쪽 중 더 적은 이동 횟수를 moves에 더함
     moves += min(left, right)
     
-    # 원소를 제거
+    # 원소를 큐에서 제거
     queue.pop(pos)
 
 # 총 이동한 횟수 출력
